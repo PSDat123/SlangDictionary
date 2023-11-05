38c060f5f804ba9379b74b8b4772705b609ed890
PSDat123 <sieudat123@gmail.com>
2023-11-05 23:36:33 +0700

    Implemented core functionalities of SlangDictionary

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,21 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         SlangDictionary dict = new SlangDictionary();
-//        System.out.println(dict.queryFromSlang("lmao"));
-//        System.out.println(dict.queryFromSlang("af"));
+//        System.out.println(dict.queryFromSlang("*_*"));
+//        System.out.println(dict.queryFromSlang("/:)"));
 //        System.out.println(dict.queryFromSlang("lol"));
-//        System.out.println(dict.queryFromSlang("fhfhg"));
-        System.out.println(dict.queryFromDefinition("brother"));
-//        System.out.printf("Hello and welcome!");
-//
-//        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-//        for (int i = 1; i <= 5; i++) {
-//
-//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-//            // for you, but you can always add more by pressing Ctrl+F8.
-//            System.out.println("i = " + i);
-//        }
+//        System.out.println(dict.queryFromSlang("HK"));
+//        System.out.println(dict.getRandomSlang());
+//        System.out.println(dict.getRandomDefinition());
+
+//        SlangQuiz quiz = dict.getSlangQuiz(4);
+//        System.out.println(quiz.getQuestion());
+//        System.out.println(quiz.getAnswers());
+//        System.out.println(quiz.getCorrectAnswer());
+
+        SlangQuiz quiz = dict.getDefinitionQuiz(4);
+        System.out.println(quiz.getQuestion());
+        System.out.println(quiz.getAnswers());
+        System.out.println(quiz.getCorrectAnswer());
     }
 }
